bd441a75b6402147dc06d5bb76d22f0c3554cab8
alx <alx@dhcppc0.(none)>
2013-02-25 22:18:51 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectManager::Application.config.secret_token = '3fea53c94f29b35bdd8436238ab2504c0a20286abec2da5deaf21253f83f43dc1b16b5f3c23dbe40de725a89599641529c0cc95a831dc16d30b0c9e03b303bab'
